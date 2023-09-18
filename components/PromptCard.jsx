f98734e77cc92dc2b022bba51e63ca7e7dd644a0
Harsh <harrypotter8401@gmail.com>
2023-09-18 20:27:28 +0530

    profileClick

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -13,16 +13,24 @@ const PromptCard = ({ post,handleTagClick,handleEdit,handleDelete }) => {
 
   const [copied, setCopied] = useState("");
 
+  const handleProfileClick=()=>{
+    if(post.creator._id === session?.user.id) return router.push("/profile");
+
+    router.push(`/profile/${post.creator._id}?name=${post.creator.username}`);
+  };
+
   const handleCopy=() =>{
     setCopied(post.prompt)
     navigator.clipboard.writeText(post.prompt);
     setTimeout(()=> setCopied(""), 3000);
-  }
+  };
 
   return (
     <div className="prompt_card">
       <div className="flex justify-between items-start gap-5">
-        <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer">
+        <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer"
+             onClick={handleProfileClick}
+        >
           <Image
             src={post.creator.image}
             alt="user_image"
@@ -47,7 +55,7 @@ const PromptCard = ({ post,handleTagClick,handleEdit,handleDelete }) => {
               ? '/assets/icons/tick.svg'
               : '/assets/icons/copy.svg'  
             }
-            alt="copy-btn"
+            alt={copied === post.prompt ? "tick_icon" : "copy_icon"}
             width={12}
             height={12}
           />
@@ -55,7 +63,8 @@ const PromptCard = ({ post,handleTagClick,handleEdit,handleDelete }) => {
       </div>
 
       <p className="my-4 font-satoshi text-sm text-gray-700">{post.prompt}</p>
-      <p className="font-inter text-sm blue_gradient cursor-pointer" onClick={()=> handleTagClick && handleTagClick(post.tag)}
+      <p className="font-inter text-sm blue_gradient cursor-pointer"
+        onClick={()=> handleTagClick && handleTagClick(post.tag)}
       >
         {post.tag}
       </p>
@@ -71,4 +80,4 @@ const PromptCard = ({ post,handleTagClick,handleEdit,handleDelete }) => {
   )
 }
 
-export default PromptCard
+export default PromptCard;
